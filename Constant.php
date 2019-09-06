dbb521066047e004e0313595036fa5fa694ed691
githubfisher <gooodfisher@126.com>
2019-09-06 20:44:08 +0800

    chore:常量类

--- Constant.php
+++ Constant.php
@@ -1,1 +1,8 @@
 <?php
+
+class Constant
+{
+    public const BASE_API_URI = 'http://openapi.touchsprite.com/api/openapi';
+    public const KEY          = 'YzFy6R1Yk7E2kMa3IIzY3t9IvDBX9On6t6JhxWU2D1jvYoylN6pZWXyppZeaKdAX';
+    public const DEVICES      = ['b6c57c4496e3180d101a325c76c95f6b9'];
+}
