96297a6fc7516fdc08e0b6ed8efe7d5047d2d03a
githubfisher <gooodfisher@126.com>
2019-09-06 20:44:18 +0800

    chore:使用常量

--- Common.php
+++ Common.php
@@ -2,19 +2,16 @@
 
 function getAuth()
 {
-    $apiUrl  = 'http://openapi.touchsprite.com/api/openapi';
-    $key     = 'YzFy6R1Yk7E2kMa3IIzY3t9IvDBX9On6t6JhxWU2D1jvYoylN6pZWXyppZeaKdAX';
-    $devices = ['b6c57c4496e3180d101a325c76c95f6b9'];
     $data    = [
         'action'  => 'getAuth',
-        'key'     => $key,
-        'devices' => $devices,
+        'key'     => Constant::KEY,
+        'devices' => Constant::DEVICES,
         'valid'   => 3600,
         'time'    => time(),
     ];
 
     $client   = new \GuzzleHttp\Client([
-        'base_uri' => $apiUrl,
+        'base_uri' => Constant::BASE_API_URI,
         'timeout'  => 30,
     ]);
     $response = $client->post('', [
