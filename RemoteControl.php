80106b012a1c5ff8b6762d99cd6c711de5c8cd81
githubfisher <gooodfisher@126.com>
2019-09-06 19:23:29 +0800

    chore:配置请求的参数

--- RemoteControl.php
+++ RemoteControl.php
@@ -1,4 +1,13 @@
 <?php
 require 'vendor/autoload.php';
 
-$apiUrl = ' http://openapi.touchsprite.com/api/openapi';
+$apiUrl  = ' http://openapi.touchsprite.com/api/openapi';
+$key     = 'YzFy6R1Yk7E2kMa3IIzY3t9IvDBX9On6t6JhxWU2D1jvYoylN6pZWXyppZeaKdAX';
+$devices = ['b6c57c4496e3180d101a325c76c95f6b9'];
+$data    = [
+    'action'  => 'getAuth',
+    'key'     => $key,
+    'devices' => $devices,
+    'valid'   => 3600,
+    'time'    => time(),
+];
